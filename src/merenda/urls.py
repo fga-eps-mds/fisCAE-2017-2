72a01419aac555593a11eebba4c8da6cc75cc952
Eduardo Júnio Veloso Rodrigues <eduardojvr10@gmail.com>
2017-09-19 16:28:33 -0300

    Primeiro template da página inicial

--- src/merenda/urls.py
+++ src/merenda/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include, url
 from django.contrib import admin
+from checklist import views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
 ]
