54dab0daabba313405b53460779fde6e4e58b3ab
aquiles <jjoseaquiless@gmail.com>
2017-09-20 22:58:56 -0300

    conserto de model

--- src/user/models.py
+++ src/user/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
+
 class advisor(models.Model):
-    name=models.CharField(max_lenght=64, null=False)
-    phone=models.CharField(max_lenght=13,null=True)
-    email=models.CharField(max_lenght=100,null=True)
-    cpf=models.CharField(max_lenght=14,null=False)
-    #endereço
-    cep=models.CharField(max_lenght=10,null=False)
-    descricao=models.CharField(max_lenght=50,null=True)
-    bairro=models.CharField(max_lenght=30,null=False)
-    municipio=models.CharField(max_lenght=30,null=False)
-    uf=models.CharField(max_lenght=2,null=False)
-    #endereço
-    
+    name = models.CharField(max_length=64, null=False)
+    phone = models.CharField(max_length=13, null=True)
+    email = models.CharField(max_length=100, null=True)
+    cpf = models.CharField(max_length=14, null=False)
+    # endereço
+    cep = models.CharField(max_length=10, null=False)
+    descricao = models.CharField(max_length=50, null=True)
+    bairro = models.CharField(max_length=30, null=False)
+    municipio = models.CharField(max_length=30, null=False)
+    uf = models.CharField(max_length=2, null=False)
+    # endereço
