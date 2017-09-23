7ea653acbcea682f3647334099dbd96242a2dd80
aquiles <jjoseaquiless@gmail.com>
2017-09-22 22:41:54 -0300

    insert template login and registro into django server
    Signed-off-by:
    Aquiles <jjoseaquiless@gmail.com>
    Miguel <miguelisdemir@gmail.com>
    Hugo <hugocatarino1@gmail.com>

--- src/user/views.py
+++ src/user/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def show_user_form(request):
+    return render(request, 'registro.html')
+
+
+def login(request):
+    return render(request, 'login.html')
