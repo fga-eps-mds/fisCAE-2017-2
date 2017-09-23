c4cbd88b0e16dd37f46aa54ab177c0fe4e84303e
Monica Galvao <mksgalvao@gmail.com>
2017-09-22 21:32:56 -0300

    Auto stash before merge of "manter_checklist" and "origin/manter_checklist"

--- src/checklist/views.py
+++ src/checklist/views.py
@@ -1,23 +1,13 @@
 from django.shortcuts import render
-from django.template import loader
-from django.http import HttpResponse
-from .models.checklist import Checklist
-from  django.template import RequestContext
-from django.template import Template
 
 
 def index(request):
-	return render(request, 'index.html')
+    return render(request, 'index.html')
 
 
-def check(request):
-	html = ''
-	newQuestion = Checklist()
-	lista = newQuestion.readQuestion()
-	for question in lista:
-		html += '<a>' + question + '</a><br>'
-	return render(request, 'check.html', {'questionList': lista})
+def home(request):
+    return render(request, 'home.html')
 
 
-
-
+def findSchool(request):
+    return render(request, 'findSchool.html')
