7a4d389b5fdbd1188b3b2df05d0b67842e72fd5d
mateusas3s <mateusaugusto-2009@hotmail.com>
2017-09-23 13:00:33 -0300

    url view_pdf_cae add

--- src/acessar_cartilha/urls.py
+++ src/acessar_cartilha/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from acessar_cartilha import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', views.acessar_cartilha, name='Acessar Cartilha')
+    url(r'^$', views.access_doc, name='access_doc'),
+    url(r'^view_pdf_cae$', views.view_pdf_cae, name='view_pdf_cae')
 ]
--- src/acessar_cartilha/views.py
+++ src/acessar_cartilha/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-def acessar_cartilha(request):
-	return render(request, 'acessar_cartilha.html')
+def access_doc(request):
+	return render(request, 'access_doc.html')
+
+def view_pdf_cae(request):
+	return render(request, 'view_pdf_cae.html')
