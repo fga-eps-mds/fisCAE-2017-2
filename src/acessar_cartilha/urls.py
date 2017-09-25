467b67ccc277a86ba3a51a3d575456f7372e051e
lbrunofidelis <l_brunofidelis@yahoo.com.br>
2017-09-24 22:39:59 -0300

    ADD link to hornbook
    Signed-off-by: lbrunofidelis <l_brunofidelis@yahoo.com.br>
    Signed-off-by: Mateusas3s <mateusaugusto-2009@hotmail.com>

--- src/acessar_cartilha/urls.py
+++ src/acessar_cartilha/urls.py
@@ -4,5 +4,5 @@ from acessar_cartilha import views
 
 urlpatterns = [
     url(r'^$', views.access_doc, name='access_doc'),
-    url(r'^view_pdf_cae/$', views.view_pdf_cae, name='view_pdf_cae')
+    url(r'^view_pdf_cae/CartilhaCAE.pdf$', views.view_pdf_cae, name='view_pdf_cae')
 ]
