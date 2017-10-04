2cd6907e9df59a743097c99eec30e73faeb34be7
aquiles <aquiles@aquiles.com>
2017-10-04 20:40:18 -0300

    remove migrations and add tests without test
    Signed-off-by: aquiles <aquiles@aquiles.com>

--- src/user/tests.py
+++ src/user/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+import unittest
+from django.test import Client
+from django.shortcuts import render
+# Create your tests here
 
-# Create your tests here.
+
+class usertest(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def registertest(self, request):
+        result = self.client.post('/user/registro', {'username': 'jhon111',
+                                                     'name': 'jhon',
+                                                     'email': 'jo@mail.com',
+                                                     'cpf': '0000',
+                                                     'cep': '333',
+                                                     'bairro': 'limoeiro',
+                                                     'municipio': 'goiania',
+                                                     'password': '123456',
+                                                     'uf': 'GO'})
+        self.assertEqual(result, render(request, 'login.html'))
+
