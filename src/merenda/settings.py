b4e321a48aecd91de17efb4ae9f42ac370e40b09
Eduardo Júnio Veloso Rodrigues <eduardojvr10@gmail.com>
2017-10-03 21:23:14 -0300

    App creation and file upload.
    Signed-off-by: Eduardo Júnio Veloso Rodrigues <eduardojvr10@gmail.com>

--- src/merenda/settings.py
+++ src/merenda/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'acessar_cartilha',
-    'checklist'
+    'checklist',
+    'acessar_documento'
 ]
 
 MIDDLEWARE = [
