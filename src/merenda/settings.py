e67fea9acb369ee0c618202d7f761040ff5e98c7
Matheus Costa <matheus.bionicle@gmail.com>
2017-10-19 16:58:26 -0200

    Add TravisCI

--- src/merenda/settings.py
+++ src/merenda/settings.py
@@ -121,3 +121,25 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+"""A basic database set-up for Travis CI.
+
+The set-up uses the 'TRAVIS' (== True) environment variable on Travis
+to detect the session, and changes the default database accordingly.
+
+Be mindful of where you place this code, as you may accidentally
+assign the default database to another configuration later in your code.
+"""
+
+if 'TRAVIS' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE':   'django.db.backends.postgresql_psycopg2',
+            'NAME':     'travisci',
+            'USER':     'postgres',
+            'PASSWORD': '',
+            'HOST':     'localhost',
+            'PORT':     '',
+        }
+    }
