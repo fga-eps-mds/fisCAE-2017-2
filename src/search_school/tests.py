14663da2bad94d90948eed409cfdb502dc2331ec
amandabezerra <amandabs_contato@hotmail.com>
2017-10-20 12:52:57 -0200

    Create tests from view functions - Alter templates
    Signed-off-by: amandabezerra <amandabs_contato@hotmail.com>

--- src/search_school/tests.py
+++ src/search_school/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .views import getItems
+from .views import getFilteredItems
 
-# Create your tests here.
+
+class SearchSchoolTest(TestCase):
+    def setUp(self):
+        pass
+
+    def testGetItems(self):
+        name = 'ESCOLA ESTADUAL PEDRO LUDOVICO TEIXEIRA'
+        state = 'TO'
+        item = getItems(name, state)
+        self.assertEquals(item[0].get('nome'), name)
+
+    def testGetFilteredItems(self):
+        name = 'Nazare'
+        state = 'DF'
+        schoolList = getFilteredItems(name, state)
+        testList = [
+            'INST EDUCACIONAL EVANGELICO NAZARENO',
+            'CR MARIA DE NAZARE',
+            'CR ESPIRITA MARIA DE NAZARE'
+            ]
+        self.assertEquals(schoolList, testList)
