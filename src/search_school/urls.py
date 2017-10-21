edd7161f1ded1202eb07f907c4bc614794766c7a
amandabezerra <amandabs_contato@hotmail.com>
2017-10-21 14:47:30 -0200

    Add validations in search school system - Alter templates of search school app
    Signed-off by: mateusas3s <mateusaugusto-2009@hotmail.com>
    Signed-off-by: amandabezerra <amandabs_contato@hotmail.com>

--- src/search_school/urls.py
+++ src/search_school/urls.py
@@ -6,6 +6,6 @@ app_name = 'search_school'
 urlpatterns = [
     url(r'^searchSchool/$', views.search, name='searchSchool'),
     url(r'^schoolForm/$', views.schoolForm, name='schoolForm'),
-    url(r'^successSchool/$', views.successSchool, name='successSchool'),
+    url(r'^redirectSchool/$', views.redirectSchool, name='redirectSchool'),
     url(r'^notLoggedIn/$', views.notLoggedIn, name='notLoggedIn'),
 ]
--- src/search_school/views.py
+++ src/search_school/views.py
@@ -5,22 +5,25 @@ import json
 
 from .forms import SchoolForm
 
+from user.models import Advisor
 
-def getItems(name, state):
+
+def getItems(name, state, city):
     items = {}
     urlBase = 'http://mobile-aceite.tcu.gov.br:80/nossaEscolaRS/'
     rest = 'rest/escolas?'
     name = 'nome=' + name.replace(" ", "%20")
     state = '&uf=' + state
+    city = '&municipio=' + city
     quantity = '&quantidadeDeItens=7'
-    url = urlBase + rest + name + state + quantity
+    url = urlBase + rest + name + state + city + quantity
     request = requests.get(url)
     items = json.loads(request.text)
     return items
 
 
-def getFilteredItems(name, state):
-    items = getItems(name, state)
+def getFilteredItems(name, state, city):
+    items = getItems(name, state, city)
     schoolList = []
     for dictionary in items:
         for (key, value) in dictionary.items():
@@ -32,35 +35,34 @@ def getFilteredItems(name, state):
 gList = []
 
 
-def getList():
-    global gList
-    return gList
-
-
 def search(request):
+    error = []
     if request.user.is_authenticated:
-        context = {}
         schoolList = []
         if request.method == 'POST':
-            schoolName = request.POST.get('school', 'Nao encontrado!')
-            if schoolName == '':
-                context['schoolName'] = ''
+            schoolName = request.POST.get('school', '')
+            if schoolName.isspace():
+                error = ['Preencha o nome.']
             else:
-                state = 'TO'
-                schoolList = getFilteredItems(schoolName, state)
+                current_user = request.user
+                userId = current_user.id
+                userObject = Advisor.objects.get(id=userId)
+                state = (userObject.uf).upper()
+                city = userObject.municipio
+                print((userObject.uf).upper(), city)
+                schoolList = getFilteredItems(schoolName, state, city)
                 global gList
                 gList = schoolList
                 if not schoolList:
-                    context['schoolName'] = 'Não encontrado'
+                    error = ['Não encontrado. Digite novamente']
                 else:
                     return HttpResponseRedirect(
                             reverse('search_school:schoolForm')
                             )
-
         return render(
                     request,
                     'search.html',
-                    {'schoolList': schoolList}
+                    {'error': error}
                 )
     else:
         return HttpResponseRedirect(reverse('search_school:notLoggedIn'))
@@ -75,7 +77,7 @@ def schoolForm(request):
                 selectedSchool = request.POST.get('school')
                 print(selectedSchool)
                 return HttpResponseRedirect(
-                                reverse('search_school:successSchool')
+                                reverse('search_school:redirectSchool')
                                 )
         else:
             schoolForm = SchoolForm(schools=gList)
@@ -87,8 +89,8 @@ def schoolForm(request):
         return HttpResponseRedirect(reverse('search_school:notLoggedIn'))
 
 
-def successSchool(request):
-    return render(request, 'schoolSuccess.html')
+def redirectSchool(request):
+    return render(request, 'redirectSchool.html')
 
 
 def notLoggedIn(request):
