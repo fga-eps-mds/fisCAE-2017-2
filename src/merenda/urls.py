be8610321705437b2f6cb203b8b4f0833933e5da
amandabezerra <amandabs_contato@hotmail.com>
2017-10-23 17:13:50 -0200

    Delete unused templates and views
    Signed-off-by: amandabezerra <amandabs_contato@hotmail.com>

--- src/merenda/urls.py
+++ src/merenda/urls.py
@@ -17,9 +17,5 @@ urlpatterns = [
         views.view_pdf_cae,
         name='view_pdf_cae'
     ),
-    url(r'^home', views.home, name='home'),
-    url(r'^findSchool', views.findSchool, name='findSchool'),
-    url(r'^formSelect', views.formSelect, name='formSelect'),
-    url(r'^tecForm', views.tecForm, name='tecForm'),
     url(r'^notLoggedIn/$', views.notLoggedIn, name='notLoggedIn'),
 ]
--- src/merenda/views.py
+++ src/merenda/views.py
@@ -1,65 +1,8 @@
 from django.shortcuts import render
 
-from checklist.models.question import Question
-from checklist.models.answer import Answer
-from checklist.models.school import School
-
 
 def index(request):
     return render(request, 'index.html')
-
-
-def home(request):
-    return render(request, 'home.html')
-
-
-def findSchool(request):
-    listSchool = School()
-    listSchool = listSchool.searchSchool()
-    foundSchool = ''
-    try:
-        if request.method == 'POST':
-            schoolName = request.POST.get('school', 'Nao encontrado!!')
-            # foundSchool = School.searchSchool(schoolName)
-            for l in listSchool:
-                if l.name == schoolName:
-                    foundSchool = schoolName
-                    return render(request, 'formSelect.html')
-        return render(
-            request,
-            'findSchool.html',
-            {'foundSchool': foundSchool, 'schoolName': schoolName}
-        )
-    except:
-        return render(
-            request,
-            'findSchool.html',
-            {'erro': 'Escola nao encontrada!!'}
-        )
-
-    # school = School(request.POST)
-    # escola = School.searchSchool(school)
-    # return render(request, 'findSchool.html', {'escola': escola})
-
-
-def formSelect(request):
-    return render(request, 'formSelect.html')
-
-
-def viewChecklist(request):
-    schools = School.objects.all()
-    answers = Answer.objects.filter(checklist_id=1)
-    questions = Question.objects.all()
-    return render(
-        request,
-        'viewchecklist.html',
-        {'answers': answers, 'questions': questions, 'schools': schools}
-    )
-
-
-def tecForm(request):
-    listQuestions = Question.listQuestionsMethod()
-    return render(request, 'tecForm.html', {'listQuestions': listQuestions})
 
 
 def access_doc(request):
