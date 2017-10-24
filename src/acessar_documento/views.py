3260acb22c9cebdddfc5ee48d20b0d70b6b774e2
eduardo <eduardojvr10@gmail.com>
2017-10-23 22:07:19 -0200

    start documentation
    Signed-off-by: aquiles <jjoseaquiless@gmail.com>

--- src/acessar_documento/views.py
+++ src/acessar_documento/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from .forms import UploadFileForm
 # Create your views here.
+from somewhere import handle_uploaded_file
+
+
+def documents(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect('/')
+    else:
+        form = UploadFileForm()
+    return render(request, 'documents.html')
