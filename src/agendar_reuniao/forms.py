6cd1f698007d52d89328d41aef3225ae3d4b4851
amandabezerra <amandabs_contato@hotmail.com>
2017-10-27 16:14:05 -0200

    Extend base to agendar_reuniao's templates
    Signed-off-by: amandabezerra <amandabs_contato@hotmail.com>

--- src/agendar_reuniao/forms.py
+++ src/agendar_reuniao/forms.py
@@ -1,7 +1,14 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea
 from agendar_reuniao.models import Agendamento
+
 
 class AgendamentoForm(ModelForm):
     class Meta:
         model = Agendamento
         exclude = ["tema"]
+        widgets = {
+            'data': Textarea(attrs={'cols': 20, 'rows': 1}),
+            'horario': Textarea(attrs={'cols': 20, 'rows': 1}),
+            'local': Textarea(attrs={'cols': 20, 'rows': 1}),
+            'observacoes': Textarea(attrs={'cols': 20, 'rows': 1}),
+        }
