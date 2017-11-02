6232639223a701efecf5b09316136c626f23f150
aquiles <jjoseaquiless@gmail.com>
2017-11-02 02:07:20 -0200

    upload test
    Signed-off-by: aquiles <jjoseaquiless@gmail.com>

--- src/acessar_documento/tests.py
+++ src/acessar_documento/tests.py
@@ -1,10 +1,21 @@
 from django.test import TestCase, Client
+from .models import Arquivos
+import os
 
 
 class TestDoc(TestCase):
 
     def setUp(self):
         self.c = Client()
+        with open('CartilhaCae.pdf', 'rb') as pdf:
+            self.data = {'title': 'cartilha', 'arquivo': pdf}
+            self.c.post('/upload_file/', self.data)
+        pdf.close
+
+    def test_upload_file(self):
+        self.assertEquals(self.data['arquivo'],
+                          Arquivos.objects.last().arquivo)
+        self.assertEqual(self.data['title'], Arquivos.objects.last().title)
 
     def test_documentsAll(self):
         response = self.c.get('/documentsAll/')
@@ -12,8 +23,9 @@ class TestDoc(TestCase):
         self.assertTemplateUsed(response, 'base_docs.html')
         self.assertTemplateUsed(response, 'documentsAll.html')
         self.assertContains(response, 'CartilhaCae.pdf')
+        self.assertNotContains(response, '.Ds_Store')
 
-    def test_upload_file(self):
+    def test_upload_file_template(self):
         response = self.c.get('/upload_file/')
         self.assertEquals(200, response.status_code)
         self.assertTemplateUsed(response, 'base_docs.html')
@@ -22,3 +34,6 @@ class TestDoc(TestCase):
     def test_viewpdf(self):
         response = self.c.get('/viewpdf/CartilhaCae.pdf', follow=True)
         self.assertEquals(200, response.status_code)
+
+    def tearDown(self):
+        os.remove('media/CartilhaCae.pdf')
