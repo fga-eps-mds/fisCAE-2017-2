2134c72e9031ee3afda2e80f830a9d932843e4d3
Marcelo Oliveira <martins.oliveira.mo@gmail.com>
2017-11-10 00:53:13 -0200

    Create model for denuciation -save data in database
    Signed-off-by: Marcelo Oliveira <martins.oliveira.mo@gmail.com>

--- src/denuncias/models.py
+++ src/denuncias/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Denunciation(models.Model):
+    user_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    description = models.TextField()
--- src/denuncias/views.py
+++ src/denuncias/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render, HttpResponseRedirect, reverse
 from django.utils import timezone
 
+from .models import Denunciation
+
 
 def denunciations(request):
+    if request.method == 'POST':
+        denunciation = Denunciation()
+        denunciation.user_name = request.POST['user_name']
+        denunciation.email = request.POST['email']
+        denunciation.description = request.POST['description']
+        denunciation.save()
+        return HttpResponseRedirect(reverse('denuncias:denunciations'))
     
-    return render(request, 'denunciations.html')
+
+
+    return render(request, 'denuncias/denunciations.html')
