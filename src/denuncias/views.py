0e18010d80eb669ab597a3e717a9062e7bc2a210
Mateus Augusto <mateusaugusto-2009@hotmail.com>
2017-11-27 22:02:42 -0200

    test new function with encode utf-8
    Signed-off-by: Mateus Augusto <mateusaugusto-2009@hotmail.com>

--- src/denuncias/views.py
+++ src/denuncias/views.py
@@ -1,8 +1,7 @@
-# -*- encoding: utf-8 -*-
-
 from django.shortcuts import render
 # from email.mime.multipart import MIMEMultipart
-# from email.mime.text import MIMEText
+from email.mime.text import MIMEText
+from email.header import Header
 import smtplib
 
 
@@ -13,47 +12,47 @@ def denunciations(request):
         state = request.POST['estado']
         end = request.POST['endereco']
         subject = request.POST['subject']
-        description = request.POST['description'] 
+        description = request.POST['description']
 #       email_to = ['ouvidoria@fnde.gov.br', 'audit@fnde.gov.br']
         email_from = 'fiscaeinfo@gmail.com'
         email_to = ['fiscaeinfo@gmail.com']
 
         # msg = MIMEMultipart('alternative')
-# 
-        # msg['From'] = email_from
-        # msg['To'] = email_to
-        # msg['Subject'] = subject
 
         text_school = 'Segue abaixo uma denuncia sobre a escola ' + school
         text_city = ' da cidade ' + city
         text_state = ' do estado ' + state
         text_end = ' situado no endereco ' + end + '\n\n'
 
-        # body = text_school + text_city + text_state + text_end + description
-# 
-        # msg.attach(MIMEText(body, 'plain'))
+        body = text_school + text_city + text_state + text_end + description
+
+        msg = MIMEText(body.encode('utf-8'), 'text', 'utf-8')
+
+        msg['From'] = email_from
+        msg['To'] = email_to
+        msg['Subject'] = Header(subject, 'utf-8')
 
         # text_school = 'Segue abaixo uma denuncia sobre a escola ' + school
         # text_city = 'da cidade ' + city
         # text_state = 'do estado ' + state
         # text_end = 'situado no endereco ' + end
-# 
-        messange = '\r\n'.join([
-                   'From: fiscaeinfo@gmail.com',
-                   'To: %s' % email_to,
-                   'Subject: %s' % subject,
-                   '',
-                   '%s' % text_school,
-                   '%s' % text_city,
-                   '%s' % text_state,
-                   '%s' % text_end,
-                   '%s' % description
-                   ])
+
+        # messange = '\r\n'.join([
+        #            'From: fiscaeinfo@gmail.com',
+        #            'To: %s' % email_to,
+        #            'Subject: %s' % subject,
+        #            '',
+        #            '%s' % text_school,
+        #            '%s' % text_city,
+        #            '%s' % text_state,
+        #            '%s' % text_end,
+        #            '%s' % description
+        #            ])
 
         smtp = smtplib.SMTP('smtp.gmail.com', 587)
         smtp.starttls()
         smtp.login(email_from, 'fiscae2017')
-        smtp.sendmail(email_from, email_to, messange)
+        smtp.sendmail(email_from, email_to, msg.as_string())
         smtp.quit()
 
         return render(request, 'index.html')
