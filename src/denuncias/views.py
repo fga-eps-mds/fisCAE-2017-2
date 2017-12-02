ba92a8a40ef0dfeb7f351f04b8e1619cbf19047a
Mateus Augusto <mateusaugusto-2009@hotmail.com>
2017-12-02 15:29:31 -0200

    function to send email done
    Signed-off-by: Mateus Augusto <mateusaugusto-2009@hotmail.com>

--- src/denuncias/views.py
+++ src/denuncias/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-# from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.header import Header
 import smtplib
+
+
+def sendmaildenunciation(request, texto, subject, emails):
+    mensagem = MIMEText(texto)
+    mensagem.set_charset('utf-8')
+    mensagem['Subject'] = subject
+    mail = smtplib.SMTP('smtp.gmail.com', 587)
+    mail.ehlo()
+    mail.starttls()
+    mail.login('fiscaeinfo@gmail.com', 'fiscae2017')
+    mail.sendmail('fiscaeinfo@gmail.com', emails, mensagem.as_string())
 
 
 def denunciations(request):
@@ -13,48 +22,17 @@ def denunciations(request):
         end = request.POST['endereco']
         subject = request.POST['subject']
         description = request.POST['description']
-#       email_to = ['ouvidoria@fnde.gov.br', 'audit@fnde.gov.br']
-        email_from = 'fiscaeinfo@gmail.com'
+        # email_to = ['ouvidoria@fnde.gov.br', 'audit@fnde.gov.br']
         email_to = ['fiscaeinfo@gmail.com']
 
-        # msg = MIMEMultipart('alternative')
-
-        text_school = 'Segue abaixo uma denuncia sobre a escola ' + school
+        text_school = 'Segue abaixo uma denúncia sobre a escola ' + school
         text_city = ' da cidade ' + city
         text_state = ' do estado ' + state
-        text_end = ' situado no endereco ' + end + '\n\n'
+        text_end = ' situado no endereço ' + end + ':\n\n'
 
-        body = text_school + text_city + text_state + text_end + description
+        text = text_school + text_city + text_state + text_end + description
 
-        # msg = MIMEText(body.encode('utf-8'), 'text', 'utf-8')
-
-        # msg['From'] = email_from
-        # msg['To'] = email_to
-        # msg['Subject'] = Header(subject, 'utf-8')
-
-        # text_school = 'Segue abaixo uma denuncia sobre a escola ' + school
-        # text_city = 'da cidade ' + city
-        # text_state = 'do estado ' + state
-        # text_end = 'situado no endereco ' + end
-
-        # messange = '\r\n'.join([
-        #            'From: fiscaeinfo@gmail.com',
-        #            'To: %s' % email_to,
-        #            'Subject: %s' % subject,
-        #            '',
-        #            '%s' % text_school,
-        #            '%s' % text_city,
-        #            '%s' % text_state,
-        #            '%s' % text_end,
-        #            '%s' % description
-        #            ])
-
-        smtp = smtplib.SMTP('smtp.gmail.com', 587)
-        smtp.ehlo()
-        smtp.starttls()
-        smtp.login(email_from, 'fiscae2017')
-        smtp.sendmail(email_from, email_to, body)
-        smtp.quit()
+        sendmaildenunciation(request, text, subject, email_to)
 
         return render(request, 'index.html')
     else:
