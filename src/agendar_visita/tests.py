468cc4ba656a004a3366d8b720806a852b715cc9
Eduardo Júnio Veloso Rodrigues <eduardojvr10@gmail.com>
2017-12-12 12:12:25 -0200

    visit scheduling test
    Signed-off-by: Eduardo Júnio Veloso Rodrigues <eduardojvr10@gmail.com>

--- src/agendar_visita/tests.py
+++ src/agendar_visita/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.test.client import Client
 from django.contrib.auth.models import User
+from .models import ScheduleVisit
 
 
 class EditScheduleTest(TestCase):
@@ -37,3 +38,30 @@ class EditScheduleTest(TestCase):
         self.assertTemplateUsed(response, 'indexScheduleVisit.html')
         
         self.assertEquals(200, response.status_code)
+
+    def test_save_visit(self):
+        school_name = 'escola teste'
+        school_code = '123456'
+        data = '12/12/2018'
+        horario = '12:12'
+        membros = 'todos'
+        estado = False
+        nome_cae = 'teste'
+        novo_agendamento = ScheduleVisit()
+        novo_agendamento.schoolName = school_name
+        novo_agendamento.schoolCode = school_code
+        novo_agendamento.date = data
+        novo_agendamento.time = horario
+        novo_agendamento.members = membros
+        novo_agendamento.status = estado
+        novo_agendamento.nome_cae_schedule = nome_cae
+        novo_agendamento.save()
+        test_agendamento = ScheduleVisit.objects.get(nome_cae_schedule=nome_cae)
+
+        self.assertEquals(school_name, test_agendamento.schoolName)
+        self.assertEquals(school_code, test_agendamento.schoolCode)
+        self.assertEquals(data, test_agendamento.date)
+        self.assertEquals(horario, test_agendamento.time)
+        self.assertEquals(membros, test_agendamento.members)        
+        self.assertEquals(estado, test_agendamento.status)
+        self.assertEquals(nome_cae, test_agendamento.nome_cae_schedule)
