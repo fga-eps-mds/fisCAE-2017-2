fdfb2b32126642edac04d928c151a8f0b4d26d12
amandabezerra <amandabs_contato@hotmail.com>
2017-12-12 00:30:40 -0200

    Correct acessar_documentos tests
    Signed-off-by: amandabezerra <amandabs_contato@hotmail.com>

--- src/agendar_reuniao/tests.py
+++ src/agendar_reuniao/tests.py
@@ -50,7 +50,7 @@ class EditScheduleTest(TestCase):
 
     def teste_template_edit_visit(self):
         self.cliente.login(username='testuser', password='12345')
-        response = self.cliente.get('/editVisit/1')
+        response = self.cliente.get('/editar-visita/1')
         self.assertEquals(301, response.status_code)
 
     def teste_template_indexScheduleMeeting(self):
--- src/agendar_visita/tests.py
+++ src/agendar_visita/tests.py
@@ -20,11 +20,11 @@ class EditScheduleTest(TestCase):
         user.set_password('12345')
         user.save()
         logged_in = self.cliente.login(username='testuser', password='12345')
-        response = self.cliente.get('/editVisit/1')
+        response = self.cliente.get('/editar-visita/1')
         self.assertEquals(301, response.status_code)
 
     def teste_template_indexScheduleVisit(self):
-        response = self.cliente.get('/indexScheduleVisit/')
+        response = self.cliente.get('/agendar-visita/')
         self.assertTemplateUsed(response, 'Base.html')
         self.assertTemplateUsed(response, 'indexScheduleVisit.html')
         self.assertEquals(200, response.status_code)
